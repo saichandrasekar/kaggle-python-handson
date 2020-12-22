a7e44388b610ef91e4cbe6f81d5a04cfee96ef02
saichandrasekar <saichandrasekar@gmail.com>
2020-12-22 06:12:26 +0530

    day01

---
+++ day01.py
@@ -0,0 +1,14 @@
+spam_amount = 0
+print(spam_amount)
+
+spam_amount = spam_amount + 4
+print(spam_amount)
+
+if (spam_amount > 0):
+    print("I got more spam")
+
+viking_Song = "Spam " * spam_amount
+print(viking_Song)
+
+print("test value", "testValue2", sep='123', end='')
+print("another string ")
